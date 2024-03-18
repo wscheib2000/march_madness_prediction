fea481c65582b82dd7b4942bde1bd60e4196c3a4
wscheib2000 <william.scheib@gatech.edu>
2024-03-18 12:26:06 -0400

    Refactored to use row of dataframe instead of object, which seems to be easier

--- team.py
+++
@@ -1,6 +0,0 @@
-class Team:
-    def __init__(self, id, name, seed, win_probs:dict) -> None:
-        self.id = id
-        self.name = name
-        self.seed = seed
-        self.win_probs = win_probs
